f013b6a7eafbeec4e9d6a111b89145fe21c1744c
Alessio De Palma <alessiodepalma84@gmail.com>
2024-08-07 09:34:17 +0000

    Minor fixes, remove change name button and add reset score to 0 feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,22 @@
 // Declare all constants
-
 const result = ["dice-1", "dice-2", "dice-3", "dice-4", "dice-5", "dice-6"];
 const humanPlayerImage = document.getElementById("humanplayer");
 const robotPlayerImage = document.getElementById("robotplayer");
 
-// Get player button element
 
+// Get player button element
 let playersbutton = document.getElementById("change-name");
 
-
-// Add event listener to player button
-
-playersbutton.addEventListener("click", function() {
-    
-    let firstplayer = prompt("Edit first player name");
-    let secondplayer = prompt("Edit second player name");
-
-    document.getElementById("firstplayer").innerText = firstplayer;
-    document.getElementById("secondplayer").innerText = secondplayer;
-
-})
-  
-
 // Get roll button element
-
 let rollButton = document.getElementById("play-game");
 
-
 // Add event listener to roll button
-
 rollButton.addEventListener("click", rollDice);
 
-
 // Function to roll the dice
-
 function rollDice() {
     let randomNumber1 = Math.floor(Math.random() * 6);
     let randomNumber2 = Math.floor(Math.random() * 6);
-
 
     humanPlayerImage.src = `assets/images/${result[randomNumber1]}.png`;
     humanPlayerImage.alt = result[randomNumber1];
@@ -45,37 +24,28 @@ function rollDice() {
     robotPlayerImage.src = `assets/images/${result[randomNumber2]}.png`;
     robotPlayerImage.alt = result[randomNumber2];
 
-
 // Score calculation
-
 if (randomNumber1 > randomNumber2) {
     winningScore();
 }
 else if (randomNumber1 < randomNumber2) {
     loseScore();
 }
-else {
-    drawScore()
 }
-}
-
 
 /**
  * Increase temporaryScoreH of 1
  * and let human player win when reaching 3 points
  */
-
 function winningScore() {
     let temporaryScoreH = parseInt(document.getElementById("temp-human-score").innerText);
     document.getElementById("temp-human-score").innerText = temporaryScoreH += 1;
     if (temporaryScoreH === 3) {
         winner();
-    }
-    
+    }   
 }
 
 // Add alert when winning the game
-
 function winner() {
     document.getElementById("endgame").classList.remove("hide");
     
@@ -88,40 +58,41 @@ function winner() {
     retryRoll()
 }
 
-// Add button for restarting the game ended
-
+/**
+ * Additional button in the alert when ending the game
+ * and allows player to play again
+ */
 function retryRoll() {
     let button = document.createElement("button");
     button.setAttribute("id","retry-roll");
     button.innerHTML = "Try to roll again!"
 
     button.addEventListener("click",function() {
-        document.getElementById("endgame").classList.add("hide");
-        rollDice()
+    document.getElementById("endgame").classList.add("hide");
+    // Reset score to o
+    document.getElementById("temp-human-score").innerText = "0";
+    document.getElementById("temp-robot-score").innerText = "0";
+
+    rollDice()
     }) 
 
     document.getElementById("endgame").appendChild(button)
 }
 
 
-
-
 /**
  * Increase temporaryScoreR of 1
  * and let robot player win when reaching 3 points
  */
-
 function loseScore() {
     let temporaryScoreR = parseInt(document.getElementById("temp-robot-score").innerText);
     document.getElementById("temp-robot-score").innerText = temporaryScoreR += 1;
     if (temporaryScoreR === 3) {
         loser();
-    }
-    
+    }    
 }
 
 // Add alert when losing the game and robot win
-
 function loser() {
     document.getElementById("endgame").classList.remove("hide");
     
@@ -133,27 +104,3 @@ function loser() {
 
     retryRoll()
 }
-
-// Attempted modification as per step 4
-/*
-function winningScore() {
-    let temporaryScoreH = parseInt(document.getElementById("temp-human-score").innerText);
-    document.getElementById("temp-human-score").innerText = temporaryScoreH += 1;
-    if (temporaryScoreH === 3) {
-        winner();
-    }
-    else {
-        loser()
-    }
-}
-
-function looseScore() {
-    let temporaryScoreR = parseInt(document.getElementById("temp-robot-score").innerText);
-    document.getElementById("temp-robot-score").innerText = temporaryScoreR += 1;
-    if (temporaryScoreR === 3) {
-        winner();
-    }
-    else {
-        loser()
-    }
-}*/
